fa74163843239e7ddcd2f37165f5b118bb416324
Nathan Patel <patnatha@med.umich.edu>
2017-07-24 10:57:28 -0400

    cleared out some bad files and also attempting to get prepare data to work

--- prepare_data/prepare_data.R
+++ prepare_data/prepare_data.R
@@ -66,14 +66,18 @@ if(file.exists(output_filename)){
 }
 
 #Load up the csv files
+print("Loading Patient B-Day")
 patient_bday = import_patient_bday(input_dir)
 
 #Load up all the encouters for the given pids
+print("Loading Patient Encounter's")
 encountersAll = import_encounter_all(patient_bday$PatientID)
 
 #Build the lab values dataset
+print("Loading Lab Values")
 lab_values = import_lab_values(input_dir)
-labValuesDplyr = inner_join(lab_values, patient_bday)
+print("LV: Calculate Time-Offest")
+labValuesDplyr = inner_join(lab_values, patient_bday, by="PatientID")
 remove(lab_values)
 labValuesDplyr = labValuesDplyr %>% mutate(
                                         timeOffset = as.numeric(
@@ -82,39 +86,41 @@ labValuesDplyr = labValuesDplyr %>% mutate(
                                     )
 
 #Exclude all the lab values that are not consistent with a grouping
-if(toInclude == "inpatient"){
-    labValuesDplyr = inner_join(labValuesDplyr, 
-                                encountersAll %>% filter(PatientClassNameSource == "Inpatient"))
-} else if(toInclude == "outpatient"){
-    labValuesDplyr = inner_join(labValuesDplyr, 
-                                encountersAll %>% filter(PatientClassNameSource == "Outpatient"))
+if(!is.null(toInclude)){
+    if(toInclude == "inpatient"){
+        print("LV: Extract Inpatients")
+        labValuesDplyr = inner_join(labValuesDplyr, 
+                                    encountersAll %>% filter(PatientClassNameSource == "Inpatient"), 
+                                    by="EncounterID")
+    } else if(toInclude == "outpatient"){
+        print("LV: Extract Outpatients")
+        labValuesDplyr = inner_join(labValuesDplyr, 
+                                    encountersAll %>% filter(PatientClassNameSource == "Outpatient"),
+                                    by="EncounterID")
+    }
 }
 
 #Get only the columns we want
-labValuesDplyr = rename(labValuesDplyr, pid = PatientID)
+print("LV: Select columns for output")
+labValuesDplyr = rename(labValuesDplyr, pid = PatientID.x)
 labValuesDplyr = rename(labValuesDplyr, l_val = VALUE)
-labValues<-labValuesDplyr %>% select(pid, l_val, timeOffset, EncounterID) 
-            %>% as.data.frame()
+labValues<-labValuesDplyr %>% select(pid, l_val, timeOffset, EncounterID) %>% as.data.frame()
 remove(labValuesDplyr)
 
 #Get the diagnosis and pair with PtID to build the timeOffset
+print("Loading Diagnoses")
 diagnoses = import_diagnoses(input_dir)
+print("DX: Calculate Time-Offset")
 diagnosis_process = inner_join(diagnoses, patient_bday, by="PatientID")
 remove(diagnoses)
-#encounter_location = import_encounter_location(input_dir)
-#encounter_earliest = encounter_location %>%
-#                        mutate(StartDate = ifelse(StartDate == "", EndDate, StartDate)) %>%
-#                        filter(StartDate != "") %>%
-#                        group_by(EncounterID) %>%
-#                        summarise(StartDate = min(as.Date(StartDate)))
-#remove(encounter_location)
 
 encounter_earliest = encountersAll %>% filter(AdmitDate != "")
 icdValuesDplyr = inner_join(diagnosis_process, encounter_earliest, by="EncounterID")
 remove(encounter_earliest)
 
 #Get the icd code assignment as an offset value
-icdValuesDplyr = rename(icdValuesDplyr, pid = PatientID)
+print("DX: Select columns for output")
+icdValuesDplyr = rename(icdValuesDplyr, pid = PatientID.x)
 icdValuesDplyr = rename(icdValuesDplyr, icd = TermCodeMapped)
 icdValuesDplyr = rename(icdValuesDplyr, icd_name = TermNameMapped)
 icdValuesDplyr = icdValuesDplyr %>%
@@ -122,16 +128,17 @@ icdValuesDplyr = icdValuesDplyr %>%
                         as.numeric(as.Date(AdmitDate)
                         -
                         as.Date(DOB)))
-icdValues<-icdValuesDplyr %>% select(pid, icd, icd_name, timeOffset, EncounterID) 
-            %>% as.data.frame()
+icdValues<-icdValuesDplyr %>% select(pid, icd, icd_name, timeOffset, EncounterID) %>% as.data.frame()
 remove(icdValuesDplyr)
 
 #Get Medications that were administered
+print("Loading Medications")
 med_admin = import_med_admin(input_dir)
 medsAdminDyplyr = med_admin %>% filter(MedicationStatus == "Given")
 remove(med_admin)
 
 #Get the administration date as an offset value
+print("MED: Calculate Time-Offset")
 medsAdminDyplyr = inner_join(medsAdminDyplyr, patient_bday, by="PatientID")
 medsAdminDyplyr = rename(medsAdminDyplyr, pid = PatientID)
 medsAdminDyplyr = rename(medsAdminDyplyr, icd = MedicationTermID)
@@ -141,10 +148,12 @@ medsAdminDyplyr = medsAdminDyplyr %>%
                         as.numeric(as.Date(DoseStartTime) 
                         - 
                         as.Date(DOB)))
-medValues<-medsAdminDyplyr %>% select(pid, icd, icd_name, timeOffset, EncounterID)
-            %>% as.data.frame()
+
+print("MED: Select columns for output")
+medValues<-medsAdminDyplyr %>% select(pid, icd, icd_name, timeOffset, EncounterID) %>% as.data.frame()
 remove(medsAdminDyplyr)
 
 #Save the massaged data
+print("SAVING RESULTS")
 save(labValues, icdValues, medValues, file=output_filename)
 
