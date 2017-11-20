8ab7a5deb8fdaf2a682c9341dfdecbc85aefd8f5
Nathan Patel <patnatha@med.umich.edu>
2017-11-20 18:04:45 -0500

    added file for scripting

---
+++ prepare_data/prepare_data.sh
@@ -0,0 +1,90 @@
+basedir="/scratch/leeschro_armis/patnatha/"
+prepfile=`ls -1 ${basedir} | tr '\n' '\0' | xargs -0 -n 1 basename`
+
+echo "=====Which directory set to prepare?====="
+theCnt=0
+finarr=()
+for tfile in $prepfile;
+do
+    if [ $tfile != 'EncountersAll' ] && [ $tfile != 'RESULT_CODES.txt' ] && [ $tfile != 'limit_results' ] && [ $tfile != 'prepared_data' ]; then
+        finarr+="${tfile}|"
+        theCnt=$((theCnt+1))
+        echo "${theCnt}) $tfile"
+    fi
+done
+read -r -p '=====Choose a number=====: ' var
+
+rawfile=''
+theCnt=0
+for tfile in $(echo $finarr | tr "|" "\n");
+do
+    if [[ $theCnt == $((var-1)) ]];
+    then
+        rawfile=$tfile
+    fi
+    theCnt=$((theCnt+1))
+done
+
+finfile=$basedir$rawfile
+
+echo "=====INCLUDE====="
+includArr=( "inpatient" "outpatient" "never_inpatient" "outpatient_and_never_inpatient" "all")
+theCnt=0
+for incArg in ${includArr[@]}
+do
+    theCnt=$((theCnt+1))
+    echo "$theCnt) $incArg"
+done
+read -r -p '=====Choose a number=====: ' var
+theCnt=0
+for incArg in ${includArr[@]}
+do
+    if [[ $theCnt == $((var-1)) ]];
+    then
+        incGrp=${includArr[$theCnt]}
+    fi
+    theCnt=$((theCnt+1))
+done
+
+echo "=====AGE====="
+echo "1) Adult"
+echo "2) Free form "
+read -r -p '=====Choose a number=====: ' var
+theage=null
+if [ $var == "1" ]
+then
+    theage="adult"
+elif [ $var == "2" ]
+then
+    read -r -p 'Enter Range: ' theage 
+else
+    echo "ERROR: 1 only"
+    exit
+fi 
+
+
+echo "=====SEX====="
+echo "1) Both"
+echo "2) Male"
+echo "3) Female"
+read -r -p '=====Choose a number=====: ' var
+thesex=""
+if [ $var == "1" ]
+then
+    thesex="both"
+elif [ $var == "2" ]
+then
+    thesex="male"
+elif [ $var == "3" ]
+then
+    thesex="female"
+else
+    echo "ERROR: 1|2|3 only"
+    exit
+fi 
+
+fincmd="qsub prepare_data.pbs -F \"--input $finfile --age $theage --sex $thesex --include $incGrp\""
+echo $fincmd
+eval $fincmd
+
+
